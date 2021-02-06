349ec9ad4d0077610d1da3c9de201b0a662c5186
shubham-00 <srpatel980@gmail.com>
2021-02-07 00:10:50 +0530

    create

---
+++ app/src/main/java/com/example/example_android_java_sqlite_crud/DatabaseHelper.java
@@ -0,0 +1,57 @@
+package com.example.example_android_java_sqlite_crud;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "database.db";
+    public static final String TABLE_NAME = "user";
+    public static final String ID = "ID";
+    public static final String NAME = "NAME";
+    public static final String EMAIL = "EMAIL";
+    public static final String SHOW = "SHOW";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME +
+                " (" +
+                ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                NAME + " TEXT, " +
+                EMAIL + " TEXT, " +
+                SHOW + " TEXT" +
+                ")";
+
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+
+        onCreate(db);
+    }
+
+    public boolean addData(String name, String email, String show) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(NAME, name);
+        contentValues.put(EMAIL, email);
+        contentValues.put(SHOW, show);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/example_android_java_sqlite_crud/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.example_android_java_sqlite_crud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper db;
+    Button add;
+    EditText name;
+    EditText email;
+    EditText show;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        db = new DatabaseHelper(this);
+        add = findViewById(R.id.addButton);
+        name = findViewById(R.id.nameInput);
+        email = findViewById(R.id.emailInput);
+        show = findViewById(R.id.showInput);
+
+        addData();
+    }
+
+    public void addData() {
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nameText = name.getText().toString();
+                String emailText = email.getText().toString();
+                String showText = show.getText().toString();
+
+                boolean insretData = db.addData(nameText, emailText, showText);
+
+                if (insretData) {
+                    Toast.makeText(MainActivity.this, "Data inserted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Something went wrong!", Toast.LENGTH_SHORT);
+                }
+            }
+        });
+    }
+}
